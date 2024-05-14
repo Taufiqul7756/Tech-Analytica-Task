7e79cfee6ffbae17ae27313771bf2e261ed1524a
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-05-14 23:25:23 +0600

    Footer and Navbar according to Figma ( Fully Responsive)

---
+++ app/components/Footer.jsx
@@ -0,0 +1,119 @@
+import Link from "next/link";
+
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import map from "../images/map.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#212529] text-white py-12 ">
+      <div className="font-bold text-4xl px-16 mb-10">LOGO</div>
+      <div className=" flex px-16">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          {/* Column 1: Get In touch */}
+          <div className="grid">
+            <h4 className="text-lg font-lg mr-10 mb-5">
+              Got Question? Call us 24/7
+            </h4>
+            <span className="font-bold mb-5">+02 055 4156</span>
+
+            <p>Register now & get you 10%</p>
+            <p>offer from first order!</p>
+
+            <span className="mt-5">Join US</span>
+
+            {/* social icon */}
+            <div className="flex text-xl mt-5 justify-start items-start  py-2 rounded-lg">
+              <Link href="#" className="text-gray-200 hover:text-white mr-4">
+                <FaFacebookF />
+              </Link>
+              <Link href="#" className="text-gray-200 hover:text-white mr-4">
+                <FaTwitter />
+              </Link>
+              <Link href="#" className="text-gray-200 hover:text-white mr-4">
+                <FaInstagram />
+              </Link>
+              <Link href="#" className="text-gray-200 hover:text-white">
+                <FaLinkedin />
+              </Link>
+            </div>
+          </div>
+
+          {/* Column 2: COMPANY */}
+          <div>
+            <h4 className="text-lg font-bold mb-4">COMPANY</h4>
+            <ul className="grid gap-5">
+              <li>
+                <Link href="/faq">About Us</Link>
+              </li>
+              <li>
+                <Link href="/terms">Career</Link>
+              </li>
+              <li>
+                <Link href="/privacy">Contact Us</Link>
+              </li>
+              <li>
+                <Link href="/blog">Start Selling </Link>
+              </li>
+              <li>
+                <Link href="/blog">Order History</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Column 3: My Account */}
+          <div className="">
+            <h4 className="text-lg font-bold mb-4">My Account</h4>
+            <ul className="grid gap-5">
+              <li>
+                <Link href="/about">Track My Order</Link>
+              </li>
+              <li>
+                <Link href="/services">View Cart</Link>
+              </li>
+              <li>
+                <Link href="/portfolio">Sign in</Link>
+              </li>
+              <li>
+                <Link href="/contact">Help</Link>
+              </li>
+              <li>
+                <Link href="/contact">Wishlist</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Column 4: Customer Services */}
+          <div>
+            <h4 className="text-lg font-bold mb-4">Customer Service</h4>
+
+            <ul className="grid gap-5">
+              <li>
+                <Link href="/about">Payment Methods</Link>
+              </li>
+              <li>
+                <Link href="/services">Money Return Policy</Link>
+              </li>
+              <li>
+                <Link href="/portfolio">Product Return</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact Seller</Link>
+              </li>
+              <li>
+                <Link href="/contact">Terms & Conditions</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [scrolling, setScrolling] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  const closeNavbar = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+
+      if (scrollTop > 50) {
+        setScrolling(false);
+      } else {
+        setScrolling(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed w-full z-10 ${
+        scrolling
+          ? "bg-[#212529]"
+          : "bg-transparent backdrop-blur-sm shadow-md "
+      }`}
+    >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex-shrink-0">
+            <Link
+              href="/"
+              className={`text-${
+                scrolling ? "white" : "black"
+              } text-xl font-bold`}
+            >
+              Logo
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                href="/"
+                className={`text-${
+                  scrolling ? "white" : "black"
+                } hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
+              >
+                Products
+              </Link>
+              <Link
+                href="/cart"
+                className={`text-${
+                  scrolling ? "white" : "black"
+                } hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
+              >
+                Cart
+              </Link>
+            </div>
+          </div>
+          <div>
+            <Link
+              href="/login"
+              className={`text-${
+                scrolling ? "white" : "black"
+              } hover:text-white bg-blue-600 px-4 py-2 rounded-md text-sm font-medium`}
+            >
+              LOGIN
+            </Link>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleNavbar}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+            >
+              {isOpen ? (
+                <FaTimes className="h-6 w-6" />
+              ) : (
+                <FaBars className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden bg-gray-800">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              onClick={closeNavbar}
+              href="/"
+              className={`text-${
+                scrolling ? "white" : "black"
+              } hover:text-white bg-slate-400 block px-3 py-2 rounded-md text-base font-medium`}
+            >
+              Products
+            </Link>
+            <Link
+              onClick={closeNavbar}
+              href="/cart"
+              className={`text-${
+                scrolling ? "white" : "black"
+              } hover:text-white bg-slate-400 block px-3 py-2 rounded-md text-base font-medium`}
+            >
+              Cart
+            </Link>
+
+            <Link
+              onClick={closeNavbar}
+              href="/login"
+              className={`text-${
+                scrolling ? "white" : "black"
+              } hover:text-white  block px-3 bg-blue-600 py-2 rounded-md text-base font-medium`}
+            >
+              LOGIN
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
