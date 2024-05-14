3e94f0b052a3d1f040964a6f46d6c4e4f52ee35b
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-05-15 01:05:23 +0600

    export baseUrl and getRequest - utility

---
+++ app/utils/service.jsx
@@ -0,0 +1,25 @@
+export const baseUrl = "https://fakestoreapi.com";
+
+export const getRequest = async (url) => {
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      let message;
+
+      if (data?.message) {
+        message = data.message;
+      } else {
+        message = data;
+      }
+
+      return { error: true, status: response.status, message };
+    }
+
+    return data;
+  } catch (error) {
+    return { error: true, message: error.message };
+  }
+};
