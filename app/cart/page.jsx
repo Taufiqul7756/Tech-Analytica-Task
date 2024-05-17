5a6971d600b06196f6c683f5931a56b4ee5bae10
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-05-18 00:12:12 +0600

    toast color added

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -5,7 +5,31 @@ import { Toaster } from "react-hot-toast";
 const CartPage = () => {
   return (
     <div className=" w-2/3 mx-auto flex justify-center">
-      <Toaster position="bottom-right" />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          className: "",
+          duration: 3000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+          success: {
+            duration: 3000,
+            theme: {
+              primary: "green",
+              secondary: "black",
+            },
+          },
+          error: {
+            duration: 3000,
+            theme: {
+              primary: "red",
+              secondary: "black",
+            },
+          },
+        }}
+      />
       <Cart />
     </div>
   );
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -11,7 +11,32 @@ const ProductPage = () => {
   if (error) return <div>Error: {error.message}</div>;
   return (
     <div>
-      <Toaster position="bottom-right" />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          className: "",
+          duration: 3000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+
+          success: {
+            duration: 3000,
+            theme: {
+              primary: "green",
+              secondary: "black",
+            },
+          },
+          error: {
+            duration: 3000,
+            theme: {
+              primary: "red",
+              secondary: "black",
+            },
+          },
+        }}
+      />
       {isLoading ? (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50 z-50">
           <div className="loader ease-linear rounded-full border-8 border-t-8 border-blue-500 h-24 w-24"></div>
